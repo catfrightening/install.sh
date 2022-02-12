73137a533c85c7957fe716883661b69110aa2560
catfrightening <96968459+catfrightening@users.noreply.github.com>
2022-02-12 21:04:11 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,32 @@
+pacman -S feh zsh keepassxc git firefox discord element-desktop alacritty xorg-server xorg-xinit neofetch
+cd /opt
+git clone https://aur.archlinux.org/yay.git
+chown -R jude:wheel ./yay
+cd yay
+makepkg -si
+cd /home/jude/
+pacman -S bspwm sxhkd pulseaudio alsa-utils alsa-tools alsa
+yay -S mullvad-vpn-bin polybar librewolf-bin
+mkdir tmp
+cd tmp 
+git clone https://github.com/catfrightening/.config/
+cd .config
+mv bspwm /home/jude/.config/bspwm
+mv sxhkd /home/jude/.config/sxhkd
+mv polybar /home/jude/.config/polybar
+mv powerlevel10k /home/jude/powerlevel10k
+mv p10k.zsh /home/jude/.p10k.zsh
+mv zshrc /home/jude/.zshrc
+mkdir /home/jude/.fonts/
+mv MesloLGS NF Bold Italic.ttf /home/jude/.fonts/
+mv MesloLGS NF Bold.ttf /home/jude/.fonts/
+mv MesloLGS NF Italic.ttf /home/jude/.fonts/
+mv MesloLGS NF Regular.ttf /home/jude/.fonts/
+mv alacritty /home/jude/.config/alacritty
+mkdir /home/jude/.config/alacritty/
+cd /home/jude
+touch xinitrc
+echo exec bspwm >> xinitrc
+echo Installed!
+
+
